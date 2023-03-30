bfbe3902a31ff9e5e31f1c62955467e874f817e4
Sala K 406 <salak406@salasfr.itm.edu.co>
2023-03-30 07:25:39 -0500

    Se crea una aplicación de consola por medio de C# que realiza una conexión con sql server a una base de datos llamada Productos y a su respectiva tabla llamada Articulos. Haciendo uso de Data.SqlCliente

---
+++ ConsolaDB/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ConsolaDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //esta es la cadena de conexion
+            SqlConnection objConectar = new SqlConnection("Data Source=localhost;Initial"+
+            " Catalog = Productos; Integrated Security = SSPI; ");
+            try
+            {
+                objConectar.Open();
+                Console.WriteLine("Se realizo la conexion...");
+                SqlCommand objComando = new SqlCommand("select * from Articulos", objConectar);
+                SqlDataReader objTabla = objComando.ExecuteReader();
+                try {
+                    while (objTabla.Read())
+                    {
+                        Console.WriteLine("" + objTabla[0] + "\t" + objTabla["nombre"]
+                            + "\t" + objTabla[2] + "\t" + objTabla[3]);
+                    }
+                    objTabla.Close();
+                } catch (SqlException e)
+            {
+                Console.WriteLine("Falló la apertura: " + e.Message);
+            }
+        }
+        catch (Exception ex) {
+            Console.WriteLine("Error en el query: " + ex.ToString());
+        }
+        Console.ReadKey();
+    }
+}
+}
